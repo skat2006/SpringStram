00bc154586bab6889c82a988611b08a529bfa03f
Andrew <skat2006@bk.ru>
2022-08-03 14:00:33 +0300

    Initial commit

---
+++ src/main/java/ru/spb/SpringStram/SpringStramApplication.java
@@ -0,0 +1,13 @@
+package ru.spb.SpringStram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStramApplication.class, args);
+	}
+
+}
